6de34dd709affc30881d5efd6ff54ed68f730325
Liam Wellacott <liamwellacott@gmail.com>
2020-02-03 13:28:40 +0000

    Izhikevich is done

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # bio-neuron
 
-Uses matplotlib and numpy
+Implements Biological/Spiking neural models, mostly purely for my own educational purposes!
+
+Currently implemented:
+- [Izhikevich model](https://www.izhikevich.org/publications/spikes.htm)
+
+Uses matplotlib and numpy.
+
--- izhikevich.py
+++ izhikevich.py
@@ -3,11 +3,20 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-initial_v = -65.0
-
 class INeuron:
+    '''
+    Implementation of Izhikevich model of a Neuron as per: https://www.izhikevich.org/publications/spikes.htm
+    '''
 
-    def __init__(self, a, b, c, d):
+    def __init__(self, a, b, c, d, initial_v=-65.0):
+        '''
+        Input
+        -----
+            - a: How quickly the neuron recovers (u) from firing, larger values lead to faster recovery.
+            - b: Sensitivity of recovery (u) to sub action potential membrane potential (v).
+            - c: Post spike reset value of membrane potential (v)
+            - d: Post spike offset to recovery (u)
+        '''
         self.a = a
         self.b = b
         self.c = c
@@ -16,58 +25,82 @@ class INeuron:
         self.v = initial_v
         self.u = self.b * self.v
 
+        self.name = "INeuron" # for plotting nicely
+        self.fired = False # record if an update leads to a spike (erased by next update)
+
     def dv_dt(self, I):
+        ''' Update of membrane potential (v) as per Izhikevich defintiion '''
         return (0.04 * self.v**2) + (5*self.v) + 140 - self.u + I
 
     def du_dt(self):
+        ''' Updae of recovery variabe (u) as per Izhikevich defintiion '''
         return self.a * ((self.b * self.v) - self.u)
 
     def update(self, I_t):
-        ''' fixed timestep of 1ms to simplify '''
-        self.v += self.dv_dt(I_t)
+        ''' 
+        Calculate and apply differential update equations, then check if threshold condition (spike) occurs.
+
+        Input
+        -----
+            - I_t: total input current at time t
+        
+        Note: fixed timestep of 1ms assumed to simplify update
+        '''
+        # Update neuron status
+        self.v += 0.5*self.dv_dt(I_t)
+        self.v += 0.5*self.dv_dt(I_t)
         self.u += self.du_dt()
-        if self.v >= 30: # Firing threshold
+
+        # Check if Firing threshold reached
+        self.fired = self.v >= 30.0
+        if self.fired: 
             self.v = self.c
             self.u += self.d
-            return True
-        return False
 
+### Neuron types as outlined in original work
 class RSNeuron (INeuron):
-
+    ''' Regular Spiking (RS)'''
     def __init__(self):
         super().__init__(0.02, 0.2, -65.0, 8.0)
+        self.name = "Regular Spiking (RS)"
 
 class IBNeuron (INeuron):
-
+    ''' Intrinsically Bursting (IB)'''
     def __init__(self):
         super().__init__(0.02, 0.2, -55.0, 4.0)
+        self.name = "Intrinsically Bursting (IB)"
 
 class CHNeuron (INeuron):
-
+    ''' Chattering (CH)'''
     def __init__(self):
         super().__init__(0.02, 0.2, -50.0, 2.0)
+        self.name = "Chattering (CH)"
 
 class FSNeuron (INeuron):
-
+    ''' Fast Spiking (FS)'''
     def __init__(self):
         super().__init__(0.1, 0.2, -65.0, 2.0)
+        self.name = "Fast Spiking (FS)"
 
 class TCNeuron (INeuron):
-
-    def __init__(self):
-        super().__init__(0.02, 0.25, -65.0, 0.05)
+    ''' Thalamo-Cortical (TC)'''
+    def __init__(self, initial_v=-65.0):
+        super().__init__(0.02, 0.25, -65.0, 0.05, initial_v=initial_v)
+        self.name = "Thalamo-Cortical (TC)"
 
 class RZNeuron (INeuron):
-
+    ''' Resonator (RZ)'''
     def __init__(self):
         super().__init__(0.1, 0.25, -65.0, 2.0)
+        self.name = "Resonator (RZ)"
 
 class LTSNeuron (INeuron):
-
+    ''' Low Threshold Spiking (LTS)'''
     def __init__(self):
         super().__init__(0.02, 0.25, -65.0, 2.0)
+        self.name = "Low Threshold Spiking (LTS)"
 
-
+### Some alternative "background current" functions for experimentation
 def constantI(t):
     return 5
 
@@ -81,70 +114,60 @@ def steppingI(t):
     return 3
 
 def thalamicI(t, i, Ne):
+    ''' 
+    As per Izhikevich description of thalamic input current, excitatory neurons have a greater background current (5 vs 2)
+    '''
     return random.normalvariate(0, 1) * (5.0 if i < Ne else 2.0)
 
-def I(t):
-    return 5
+### Modes of running model
+def oneNeuron(neuron, sim_length, current_func):
 
-def AllNeuron():
-    neurons = [RSNeuron(), IBNeuron(), CHNeuron(), FSNeuron(), TCNeuron(), RZNeuron(), LTSNeuron()] 
-
-    Vms = []
-    spikes = []
-    for n in neurons:
-        Vms.append([n.v])
-        spikes.append([0])
-
-    ts = range(250)
-
-    fig, ax = plt.subplots(7, 2)
-    
-    for t in ts[1:]:
-        for i, n in enumerate(neurons):
-            spike = n.update(I(t))
-
-            Vms[i].append(n.v)
-            spikes[i].append(1 if spike else 0)
-
-    for i in range(len(neurons)):  
-        ax[i][0].plot(ts, Vms[i])
-        ax[i][1].plot(ts, spikes[i])
-    plt.show()
-
-def oneNeuron():
-    global initial_v
-
-    initial_v = -65.0
-    neuron = RZNeuron()
+    # record initial state
     Vms = [neuron.v]
     spikes = [0]
 
-    fig, ax = plt.subplots(1, 2)
-    ts = range(250)
-
+    # run for 
+    ts = range(sim_length)
     for t in ts[1:]:
-        spike = neuron.update(steppingI(t))
+        neuron.update(current_func(t))
         
-        Vms.append(neuron.v)
-        spikes.append(1 if spike else 0)
+        if neuron.fired:
+            Vms.append(30) # to normalise graph
+            spikes.append(1)
+        else:
+            Vms.append(neuron.v)
+            spikes.append(0)
  
-    ax[0].plot(ts, Vms)
-    ax[1].plot(ts, spikes)
+    fig, (v_ax, spike_ax) = plt.subplots(1, 2)
+    v_ax.plot(ts, Vms)
+    v_ax.set_xlabel("time (ms)")
+    v_ax.set_ylabel("membrane potential (v)")
+    
+    spike_ax.plot(ts, spikes)
+    spike_ax.set_xlabel("time (ms)")
+    spike_ax.set_ylabel("Spike occurrence")
+
+    fig.suptitle(neuron.name)
     plt.show()
 
-def network():
+def allNeuron(sim_length):
+    neurons = [RSNeuron(), IBNeuron(), CHNeuron(), FSNeuron(), TCNeuron(initial_v=-63.0), TCNeuron(initial_v=-87.0), RZNeuron(), LTSNeuron()] 
+    for n in neurons:
+        oneNeuron(n, sim_length, constantI)
 
-    random.seed(1)
+def network(Ne, Ni, sim_length):
 
-    Ne = 900
-    Ni = 100
+    random.seed(1) # for repeatability
+
     total = Ne + Ni
-
     rx = [ random.random() for _ in range(total) ]
-    neurons = []
-    S = []
+
+    ## Create network
+    neurons = [] 
+    S = [] # synaptic connection strengths
     for i, r in enumerate(rx):
-        # create neuron
+
+        # create neuron as per ranges defined in Izhikevich
         if i < Ne: # is excitatory
             neurons.append(INeuron(0.02, 0.2, -65.0 + (15.0 * (r**2)), 8.0 - (6.0 * (r**2))))
         else:
@@ -159,31 +182,36 @@ def network():
                 ws[j] = ws[j] * -1 # inhibit
         S.append(ws)
 
-    ts = range(1000)
+    ## Run network
+    ts = range(sim_length)
     
-    fires = [0] * total
-    spikes = [ fires ]
+    fires_last = [0] * total 
+    spikes = [ fires_last ]
     for t in ts:
 
-        fires_t = []
+        fires_now = []
         for i, n in enumerate(neurons):
 
+            # calculate current input current
             I_t = thalamicI(t, i, Ne)
             for j in range(total):
-                if fires[j]: # if fired last time
-                    I_t += S[j][i]
-   
-            spike = n.update(I_t)
-            fires_t.append(1 if spike else 0)
+                if fires_last[j]:
+                    I_t += S[j][i] # each fire injects some current (scaled by connection weight)
 
-        spikes.append(fires_t)
-        fires = fires_t
+            # update neuron and record spikes
+            n.update(I_t)
+            fires_now.append(1 if n.fired else 0)
+
+        spikes.append(fires_now)
+        fires_last = fires_now
     
     plt.matshow(spikes)
     plt.show()
 
 
 if __name__ == "__main__":
-    #oneNeuron()
-    #AllNeuron()
-    network()
+    # delete/add below as required
+
+    #oneNeuron(RSNeuron(), 250, constantI)
+    #allNeuron(250)
+    network(800, 200, 500)
